62363c7e75673fc03b075dfb36e7dc096eedf987
hoshikwyw <khaingwutyiwin1712@gmail.com>
2023-09-15 16:07:14 +0630

    discover ui

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,18 +1,40 @@
 import React from 'react'
 import PlayPause from './PlayPause'
 import {playPause, setActiveSong} from '../redux/services/PlayerSlice'
+import { Link } from 'react-router-dom';
 
-const SongCard = ({song,i}) => {
-  const activeSong = 'hello';
+const SongCard = ({song, i, isPlaying, activeSong, data}) => {
+
+  // const activeSong = 'hello';
+
+  const handlePauseBtn = () => {
+
+  }
+
+  const handlePlayBtn = () => {
+
+  }
+
   return (
     <div className=' flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer'>
       <div className=" relative w-full h-60 group">
         <div className={` absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${activeSong?.title === song.title ? 'flex bg-black bg-opacity-70' : 'hidden'}`}>
-          <PlayPause />
+          <PlayPause song={song} isPlaying={isPlaying} activeSong={activeSong} handlePause={handlePauseBtn} handlePlay={handlePlayBtn} />
         </div>
         <img src={song.images?.coverart} alt="song_image" className=' h-60' />
       </div>
-      
+      <div className=" mt-4 flex flex-col">
+        <p className=' font-semibold text-lg text-white truncate'>
+          <Link to={`/songs/${song?.key}`}>
+          {song.title}
+          </Link>
+        </p>
+        <p className=' text-sm truncate text-gray-300 mt-1'>
+          <Link to={song.artists? `/artists/${song?.artists[0]?.adamid}` : '/top-artists' }>
+          {song.subtitle}
+          </Link>
+        </p>
+      </div>
     </div>
   )
 }
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -5,13 +5,20 @@ import axios from "axios";
 import { useGetChartTracksQuery } from "../redux/services/dataFetch";
 import Loader from "../components/Loader";
 import Error from "../components/Error";
+import { useDispatch, useSelector } from "react-redux";
+
+
 
 const Discover = () => {
+  const dispatch = useDispatch()
+  const { activeSong, isPlaying} = useSelector((state) => (state.player))
   const { data, isFetching, isLoading, error } = useGetChartTracksQuery();
   const genreTitle = "Pop";
   console.log(data);
 
   if (isFetching) {return <Loader />};
+
+  if (isLoading) {return <Loader />};
 
   if (error) {return <Error />};
 
@@ -31,7 +38,7 @@ const Discover = () => {
       </div>
       <div className=" flex flex-wrap justify-start md:justify-center gap-8">
         {data.tracks?.map((songs, i) => (
-          <SongCard key={songs.key} song={songs} i={i} />
+          <SongCard key={songs.key} song={songs} data={data.tracks} i={i} isPlaying={isPlaying} activeSong={activeSong} />
         ))}
       </div>
     </div>
